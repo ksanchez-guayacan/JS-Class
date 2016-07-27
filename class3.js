1c6f71462744066becb22c9d1570461931380478
ksanchez-guayacan <katesanchez-roig@Rosanas-MacBook-Air.local>
2016-07-26 21:10:47 -0400

    Class 3 July 26
    Notes and Exercise 1 and 2

---
+++ class3.js
@@ -0,0 +1,21 @@
+//This is a comment
+
+//Data Types:
+//Number eg: 1,2,
+//String eg: "Hello", 'Hello'
+//Boolean eg: true, false
+
+//Find out why Array is object in Node 6
+//Complex data types
+//Array eg: [1, "hello", false]
+//Object eg:{ key:value }
+
+//fuction
+function hello() {
+  console.log("Hello from Codetrotters");
+  console.log("Nice to meet you ", name);
+}
+
+//Print to current output eg: stdin, webconsole
+
+console.log(1);
---
+++ exercise1.js
@@ -0,0 +1,8 @@
+console.log(16 + 18.9);
+
+var a = 25
+var b = 45
+var c = 65
+var d = 5
+
+console.log(a + b + c / d)
